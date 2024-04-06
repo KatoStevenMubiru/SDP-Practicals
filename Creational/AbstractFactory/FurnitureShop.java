e4aeb60daac59d5a2c047004fce4b9776224c494
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-06 06:29:32 +0300

    Create FurnitureShop.java

---
+++ Creational/AbstractFactory/FurnitureShop.java
@@ -0,0 +1,14 @@
+//Client code 
+
+package Creational.AbstractFactory;
+
+public class FurnitureShop {
+        private Chair chair;
+        private Sofa sofa;
+        private CoffeeTable coffeeTable;
+
+        public FurnitureShop(FurnitureFactory factory){
+            chair = factory.createCh
+        }
+    
+}
