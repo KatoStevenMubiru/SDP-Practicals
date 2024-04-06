cc3b483bf15d9853c845cb250eba399cca5dddc8
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-06 06:26:31 +0300

    Update FurnitureFactory.java

---
+++ Creational/AbstractFactory/FurnitureFactory.java
@@ -0,0 +1,10 @@
+//Abstract Factory Interface
+
+package Creational.AbstractFactory;
+
+public interface FurnitureFactory {
+       Chair createChair();
+       Sofa createSofa();
+       CoffeeTable createCoffeeTable();
+    
+}
