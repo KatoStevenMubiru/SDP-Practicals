ae9540f62aa938cf46e1f6aaf2980f285c675abe
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-06 06:26:33 +0300

    Create ModernFurnitureFactory.java

---
+++ Creational/AbstractFactory/ModernFurnitureFactory.java
@@ -0,0 +1,15 @@
+//Concrete Factory
+
+package Creational.AbstractFactory;
+
+public class ModernFurnitureFactory implements FurnitureFactory {
+    public Chair createChair(){
+        return new ModernChair();
+    }
+    public Sofa createSofa(){
+        return new ModernSofa();
+    }
+    public CoffeeTable createCoffeeTable(){
+        return new ModernCoffeeTable();
+    }
+}
