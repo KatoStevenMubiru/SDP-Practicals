cb9177ea0c5d8bdd375165a9176f6083207b48a2
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-06 06:35:30 +0300

    Update Application.java

--- Creational/AbstractFactory/Application.java
+++ Creational/AbstractFactory/Application.java
@@ -4,5 +4,10 @@ public class Application{
     public static void main(String[] args) {
         FurnitureFactory factory;
         //Factory type
+        factory = new ModernFurnitureFactory();
+
+        //we shop what we want below
+        FurnitureShop shop = new FurnitureShop(factory);
+        shop.showcaseFurniture();
     }
 }
