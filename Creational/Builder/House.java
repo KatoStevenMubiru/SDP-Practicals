7b97419aaf2ef8ebf9b212f25774d3e7e8c19873
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-07 07:17:59 +0300

    Update House.java

--- Creational/Builder/House.java
+++ Creational/Builder/House.java
@@ -1,3 +1,15 @@
 //The Product
 //This is the complex product we are making
 
+ class House {
+
+    //parts of the house
+    private String foundation;
+    private String structure;
+    private String roof;
+
+    public void setFoundation(String foundation){
+        this.foundation = foundation;
+    }
+    public 
+}
