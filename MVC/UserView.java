9b7a63477493847ebd71d36cc44c264b693f94ff
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-09 06:36:46 +0300

    Update UserView.java

--- MVC/UserView.java
+++ MVC/UserView.java
@@ -2,5 +2,9 @@ package MVC;
 
 public class UserView {
     
-    
+    public void printUserDetails(String name, String email){
+        System.out.println("These are the user details");
+        System.out.println("Name :" +name);
+        System.out.println("Email :" +email);
+    }
 }
