966aaedbd46612ed95df35a3d5454f5a5d27834f
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-09 06:36:44 +0300

    Update UserController.java

--- MVC/UserController.java
+++ MVC/UserController.java
@@ -1,6 +1,12 @@
 package MVC;
 
 public class UserController {
-    
-    
+    private UserModel model;
+    private UserView view;
+
+    public UserController(UserModel model, UserView view){
+        this.model = model;
+        this.view = view;
+    }
+    public void UserModel(U)
 }
