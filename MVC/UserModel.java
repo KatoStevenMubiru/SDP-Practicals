7a14e9cc11dcd137c0927ebc1de6af4ebe4a36b4
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-09 06:30:28 +0300

    Create UserModel.java

---
+++ MVC/UserModel.java
@@ -0,0 +1,19 @@
+package MVC;
+
+public class UserModel {
+    public String name;
+    public String email;
+
+    public void setName(){
+        this.name = name;
+    }
+    public void setEmail(){
+        this.email = email;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+}
