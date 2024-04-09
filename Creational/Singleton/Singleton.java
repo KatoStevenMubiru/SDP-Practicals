f804b15e7a37d2fd16fd1d36a90c5850fba74437
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-09 04:54:40 +0300

    Update Singleton.java

--- Creational/Singleton/Singleton.java
+++ Creational/Singleton/Singleton.java
@@ -4,5 +4,21 @@ public class Singleton {
     // must be private
     private static volatile Singleton instance;
 
+    private Singleton(){
 
+    }
+
+    public static Singleton getInstance(){
+
+        if(instance == null){
+
+            synchronized(Singleton.class){
+
+                if(instance = null){
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
 }
