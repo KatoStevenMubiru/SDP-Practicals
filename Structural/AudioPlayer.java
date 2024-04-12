5ba2243d04e1599e97be4c8f7287460dd922574d
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-12 06:00:27 +0300

    Update AudioPlayer.java

--- Structural/AudioPlayer.java
+++ Structural/AudioPlayer.java
@@ -8,7 +8,13 @@ public class AudioPlayer implements MediaPlayer {
         if(audioType.equalsIgnoreCase("mp3")){
             System.out.println("Playing mp3 file. Name: " + fileName);
         }
-        else if(audioType.equalsIgnoreCase("vlc"))
+        else if(audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")){
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(audioType, fileName);
+        }else{
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+
     }
     
 }
