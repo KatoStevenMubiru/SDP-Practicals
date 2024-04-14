db4da826d16e870b14669e0fb2b2fba1fa776ad6
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-14 06:05:06 +0300

    Create MediaAdapter.java

---
+++ Structural/Adapter/MediaAdapter.java
@@ -0,0 +1,21 @@
+package Structural;
+
+public class MediaAdapter implements MediaPlayer{
+    AdvancedMediaPlayer advancedMediaPlayer;
+
+    public MediaAdapter(String audioType){
+        if(audioType.equalsIgnoreCase("vlc")){
+            advancedMusicPlayer = new VlcPlayer();
+        }else if(audioType.equalsIgnoreCase("mp4")){
+            advancedMusicPlayer = new Mp4Player();
+        }
+    }
+    public void play(String audioType, String fileName){
+        if(audioType.equalsIgnoreCase("vlc")){
+            advancedMusicPlayer.playVlc(fileName);
+        }else if(audioType.equalsIgnoreCase("mp4")){
+            advancedMediaPlayer.playMp4(fileName);
+        }
+    }
+    
+}
