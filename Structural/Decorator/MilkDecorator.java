39b34a1bddf0a6946872bd0cbffefc89ac69db68
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-16 06:59:22 +0300

    Update MilkDecorator.java

--- Structural/Decorator/MilkDecorator.java
+++ Structural/Decorator/MilkDecorator.java
@@ -1,14 +1,14 @@
 package Structural.Decorator;
 
 public class MilkDecorator extends CoffeeDecorator{
-    private MilkDecorator deocoratedCoffee;
+    
 	public MilkDecorator(Coffee coffee){
         super(coffee);
     }
 
     @Override
     public String getDescription(){
-        return deocoratedCoffee.getDescription() + ", Milk";
+        return decoratedCoffee.getDescription() + ", Milk";
     }
     @Override
     public double getCost(){
