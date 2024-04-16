ddf25a3845c6028b6ee5cd446e00942d19d61e28
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-16 07:09:45 +0300

    Update CoffeeShop.java

--- Structural/Decorator/CoffeeShop.java
+++ Structural/Decorator/CoffeeShop.java
@@ -2,7 +2,13 @@ package Structural.Decorator;
 
 public class CoffeeShop {
     public static void main(String[] args) {
-        Coffee myCoffee
+        Coffee myCoffee = new BasicCoffee();
+        System.out.println(myCoffee.getDescription() + " Cost: $" + myCoffee.getCost());
+
+        myCoffee = new MilkDecorator(myCoffee);
+        System.out.println(myCoffee.getDescription() + " Cost: $" + myCoffee.getCost());
+
+        myCoffee = new SugarDecorator(myCoffee);
+        System.out.println(myCoffee.getDescription() + " Cost: $" + myCoffee.getCost());
     }
-    
 }
