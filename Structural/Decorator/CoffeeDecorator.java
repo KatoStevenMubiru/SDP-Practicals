1bbb44e409ef2cec2b2b2fd7c2f97ed53dcddbcd
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-16 06:53:25 +0300

    Update CoffeeDecorator.java

--- Structural/Decorator/CoffeeDecorator.java
+++ Structural/Decorator/CoffeeDecorator.java
@@ -3,5 +3,9 @@ package Structural.Decorator;
 public abstract class CoffeeDecorator implements Coffee {
     // initialize coffee object
     protected Coffee deocoratedCoffee;
+
+    public CoffeeDecorator(Coffee coffee){
+        this.deocoratedCoffee = coffee;
+    }
     
 }
