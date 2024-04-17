6bc62f26f667294c63fb46ea6fca49c9a705699e
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-17 07:26:01 +0300

    Create DvdPlayer.java

---
+++ Structural/Facade/DvdPlayer.java
@@ -0,0 +1,11 @@
+package Facade;
+
+public class DvdPlayer {
+    void play(String movie){
+        System.out.println("Playing movie: " + movie);
+    }
+    void stop(){
+        System.out.println("Stopped playing DVD. ");
+    }
+    
+}
