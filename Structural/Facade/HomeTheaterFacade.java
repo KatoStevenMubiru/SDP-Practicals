2201fd2904c67165718b9fcb8dc47eb8ccf33798
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-17 07:33:55 +0300

    Update HomeTheaterFacade.java

--- Structural/Facade/HomeTheaterFacade.java
+++ Structural/Facade/HomeTheaterFacade.java
@@ -1,6 +1,11 @@
 package Facade;
 
 class HomeTheaterFacade {
+    private Projector projector;
+    private DvdPlayer dvdPlayer;
+    private SoundSystem soundSystem;
+    private Lights lights;
 
+    
     
 }
