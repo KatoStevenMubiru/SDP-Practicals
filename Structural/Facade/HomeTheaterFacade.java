1ba82a431d46189daef5fa70235413dd90fcfdd2
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-17 07:45:04 +0300

    Update HomeTheaterFacade.java

--- Structural/Facade/HomeTheaterFacade.java
+++ Structural/Facade/HomeTheaterFacade.java
@@ -6,6 +6,16 @@ class HomeTheaterFacade {
     private SoundSystem soundSystem;
     private Lights lights;
 
-    public HomeTheaterFacade(Projector projector, DvdPlayer dvdPlayer, SoundSystem soundSystem, Lights)
+    public HomeTheaterFacade(Projector projector, DvdPlayer dvdPlayer, SoundSystem soundSystem, Lights lights){
+        this.projector = projector;
+        this.dvdPlayer = dvdPlayer;
+        this.soundSystem = soundSystem;
+        this.lights = lights;
+    }
+    public void watchMovie(String movie){
+        System.out.print("Get ready to watch a movie...");
+        lights.dim(10);
+        projector.on();
+    }
     
 }
