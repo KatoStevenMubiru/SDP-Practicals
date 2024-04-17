686815e3a678b799be0558b762dad5f72e6f5b81
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-17 07:28:52 +0300

    Create SoundSystem.java

---
+++ Structural/SoundSystem.java
@@ -0,0 +1,13 @@
+
+class SoundSystem {
+     void on(){
+        System.out.println("Sound system turned on.");
+     }
+     void off(){
+        System.out.println("Sound system turned off.");
+     }
+     void setVolume(int level){
+        System.out.println("Sound system volume set to " + level);
+     }
+    
+}
