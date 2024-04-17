7777658b4e12d72ac00b8a7fa6ed0d68df531564
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-17 07:50:42 +0300

    Create Main.java

---
+++ Structural/Facade/Main.java
@@ -0,0 +1,18 @@
+package Facade;
+
+/**
+ * Main
+ */
+public class Main {
+  public static void main(String[] args) {
+    Projector projector = new Projector();
+    DvdPlayer dvdPlayer = new DvdPlayer();
+    SoundSystem soundSystem = new SoundSystem();
+    Lights lights = new Lights();
+
+    HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade(projector, dvdPlayer, soundSystem, lights);
+    homeTheaterFacade.watchMovie("Inception");
+
+  }
+    
+}
