8db6cb0d333799065e2bbdc3bea6517dabfb2261
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-18 07:52:14 +0300

    Update CircleFactory.java

--- Structural/Flyweight/CircleFactory.java
+++ Structural/Flyweight/CircleFactory.java
@@ -2,5 +2,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CircleFactory{
-    private static final Map<String, Circle> circleMap
+    private static final Map<String, Circle> circleMap = new HashMap<>();
+    
+    public static Circle getCircle(String color){
+        Circle circle = circleMap.get(color);
+        if(circle ==null){
+           circle = new ConcreteCircle(color);
+           circleMap.put(color, circle); 
+        }
+        return circle;
+    }
 }
