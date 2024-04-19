b38bbd425209197a70c917a90f58587bf5c940dd
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-19 05:33:57 +0300

    Update ClientCode.java

--- Structural/Proxy/ClientCode.java
+++ Structural/Proxy/ClientCode.java
@@ -7,6 +7,9 @@ public class ClientCode {
 
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount(10000);
-        Payment paymentMethod = new CreditC
+        Payment paymentMethod = new CreditCard(bankAccount);
+
+        paymentMethod.pay(3000);
+        paymentMethod.pay(6000);
     }
 }
