a3873c58a6b52450e83377c1531ccf1947c2b7a1
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-19 05:33:21 +0300

    Create ClientCode.java

---
+++ Structural/Proxy/ClientCode.java
@@ -0,0 +1,12 @@
+package Proxy;
+
+/**
+ * ClientCode
+ */
+public class ClientCode {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount(10000);
+        Payment paymentMethod = new CreditC
+    }
+}
