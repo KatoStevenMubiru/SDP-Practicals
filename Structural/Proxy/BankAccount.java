603e7134850016f9ddde9f0986abe45c42e7d76e
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-19 05:24:40 +0300

    Update BankAccount.java

--- Structural/Proxy/BankAccount.java
+++ Structural/Proxy/BankAccount.java
@@ -13,7 +13,7 @@ public class BankAccount implements Payment {
         balance -= amount;
         System.out.println("Paid " + amount + " using bank account. Remaining balance: " + balance);
     } else{
-        System.out.println("Insufficient");
+        System.out.println("Insufficient funds");
     }
    }
 }
