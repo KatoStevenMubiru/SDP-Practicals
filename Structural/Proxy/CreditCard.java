465e20482f01ebde0d397398d01d0f2feb3e8e9f
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-19 05:26:51 +0300

    Update CreditCard.java

--- Structural/Proxy/CreditCard.java
+++ Structural/Proxy/CreditCard.java
@@ -4,7 +4,11 @@ package Proxy;
  * CreditCard
  */
 public class CreditCard  implements Payment{
-    
+    private BankAccount bankAccount;
+
+    public CreditCard(BankAccount bankAccount){
+        this.bankAccount = bankAccount;
+    }
 
     
 }
