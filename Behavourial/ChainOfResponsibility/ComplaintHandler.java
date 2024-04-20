73a4d047aedad2122df4df66c2d3f1ca931c86cc
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-20 06:48:17 +0300

    Update ComplaintHandler.java

--- Behavourial/ChainOfResponsibility/ComplaintHandler.java
+++ Behavourial/ChainOfResponsibility/ComplaintHandler.java
@@ -3,6 +3,12 @@ package Behavourial.ChainOfResponsibility;
 public class ComplaintHandler implements Handler{
      public Handler nexHandler;
      
-     @
+     @Override
+     public void setNext(Handler handler){
+        this.nexHandler = handler;
+     }
+
+     @Override
+     public 
     
 }
