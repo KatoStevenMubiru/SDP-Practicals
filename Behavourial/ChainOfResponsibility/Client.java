eae0edf097aefbc9b898cea870f8b13e8e36322a
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-20 06:53:43 +0300

    Update Client.java

--- Behavourial/ChainOfResponsibility/Client.java
+++ Behavourial/ChainOfResponsibility/Client.java
@@ -3,7 +3,14 @@ package Behavourial.ChainOfResponsibility;
 public class Client {
     public static void main(String[] args){
         Handler spam = new SpamHandler();
-        Handler
+        Handler fan = new FanMailHandler();
+        Handler complaint = new ComplaintHandler();
+
+        spam.setNext(fan);
+        fan.setNext(complaint);
+        complaint.setNext(spam);
+
+        sp
     }
     
 }
