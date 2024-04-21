5e2b1d94e87a583c995c732b0544854b49914d07
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-21 06:19:11 +0300

    Update Application.java

---
+++ Behavourial/Command/Application.java
@@ -0,0 +1,15 @@
+package Behavourial.Command;
+
+/**
+ * Application
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        Document document = new Document();
+        Command copy = new CopyCommand(document);
+        Command paste = new PasteCommand(document);
+
+        Button copyButton = new Button(copy);
+    }
+}
