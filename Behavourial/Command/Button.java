0e4bd57a04693ece02e05860cde37b418455168f
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-21 06:15:23 +0300

    Update Button.java

---
+++ Behavourial/Command/Button.java
@@ -0,0 +1,14 @@
+package Behavourial.Command;
+
+/**
+ * Button
+ */
+public class Button {
+
+    private Command command;
+
+    public Button(Command command){
+        this.command = command;
+    }
+
+}
