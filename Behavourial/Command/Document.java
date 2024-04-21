9d9349715a4a4e1791a9eae90cede58c5a32caa5
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-21 06:13:45 +0300

    Update Document.java

---
+++ Behavourial/Command/Document.java
@@ -0,0 +1,14 @@
+package Behavourial.Command;
+
+/**
+ * Document
+ */
+public class Document {
+
+    public void copy(){
+        System.out.println("Document copied to clipboard.");
+    }
+    public void paste(){
+        System.out.println("")
+    }
+}
