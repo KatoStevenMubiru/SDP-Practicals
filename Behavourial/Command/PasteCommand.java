0e8e25ceeaddbd3a3c63faa7dd92c1a00e87d496
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-21 06:12:02 +0300

    Update PasteCommand.java

--- Behavourial/Command/PasteCommand.java
+++ Behavourial/Command/PasteCommand.java
@@ -8,5 +8,12 @@ import javax.swing.text.Document;
 public class PasteCommand implements Command{
     private Document document;
 
+    public PasteCommand(Document document){
+        this.document = document;
+    }
+    @Override
+    public void execute(){
+        document.paste();
+    }
     
 }
