585a5469b8495bca048373483c12fdd830e8c577
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-21 06:10:44 +0300

    Create PasteCommand.java

---
+++ Behavourial/Command/PasteCommand.java
@@ -0,0 +1,12 @@
+package Behavourial.Command;
+
+import javax.swing.text.Document;
+
+/**
+ * PasteCommand
+ */
+public class PasteCommand implements Command{
+    private Document document;
+
+    
+}
