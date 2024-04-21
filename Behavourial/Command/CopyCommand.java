303e9a702ff8d100dd1df660277c08ac24b4e2da
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-21 06:08:01 +0300

    Create CopyCommand.java

---
+++ Behavourial/Command/CopyCommand.java
@@ -0,0 +1,13 @@
+package Behavourial.Command;
+
+import javax.swing.text.Document;
+
+public class CopyCommand implements Command{
+    private Document document;
+
+    public CopyCommand(Document document){
+        this.docu
+    }
+
+    
+}
