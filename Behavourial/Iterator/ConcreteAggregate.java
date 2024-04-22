599d21066a0f2aef2dd9028616e79cc41551c31d
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-22 05:26:37 +0300

    Create ConcreteAggregate.java

---
+++ Behavourial/Iterator/ConcreteAggregate.java
@@ -0,0 +1,10 @@
+package Behavourial.Iterator;
+
+import java.util.ArrayList;
+
+public class ConcreteAggregate implements Aggregate{
+    private List<Integer> items = new ArrayList<>();
+
+    
+    
+}
