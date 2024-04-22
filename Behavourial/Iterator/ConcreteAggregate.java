5091cf838059cf037f932ca1797e0e6abb2fee15
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-22 05:27:35 +0300

    Update ConcreteAggregate.java

--- Behavourial/Iterator/ConcreteAggregate.java
+++ Behavourial/Iterator/ConcreteAggregate.java
@@ -1,10 +1,16 @@
 package Behavourial.Iterator;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ConcreteAggregate implements Aggregate{
     private List<Integer> items = new ArrayList<>();
 
+    public void add(Integer item){
+        items.add(item);
+    }
+
+    @Override
     
     
 }
