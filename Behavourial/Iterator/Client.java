a69815d2fc9a4a57e12009d17307c44bbde577c6
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-22 05:32:55 +0300

    Update Client.java

--- Behavourial/Iterator/Client.java
+++ Behavourial/Iterator/Client.java
@@ -1,5 +1,15 @@
 package Behavourial.Iterator;
 
 public class Client {
-    
+    public static void main(String[] args) {
+        ConcreteAggregate aggregate = new ConcreteAggregate();
+        aggregate.add(1);
+        aggregate.add(2);
+        aggregate.add(3);
+
+        Iterator<Integer> iterator = aggregate.createIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
