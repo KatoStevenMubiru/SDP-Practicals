12a7ea2ad6743a71e5e24cb8942b8b764e059c33
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-23 06:50:07 +0300

    Update TextField.java

--- Behavourial/Mediator/TextField.java
+++ Behavourial/Mediator/TextField.java
@@ -2,5 +2,15 @@ package Behavourial.Mediator;
 
 public class TextField extends Component{
     private boolean visible = false;
+
+    public TextField(DialogMediator mediator){
+        super(mediator);
+    }
+
+    public void setVisible(boolean visible){
+        this.visible = visible;
+
+        System.out.println("Text field visibility: " + visible);
+    }
     
 }
