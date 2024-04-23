171562f69eb4e79c0107b9b3079716beb20669a1
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-23 06:24:43 +0300

    Update ProfileDialog.java

--- Behavourial/Mediator/ProfileDialog.java
+++ Behavourial/Mediator/ProfileDialog.java
@@ -2,6 +2,8 @@ package Behavourial.Mediator;
 
 public class ProfileDialog implements DialogMediator{
     private Textfield dogNameField;
+    private Checkbox hasDogCheckbox;
+    private Button submitButton;
     
 }
  
