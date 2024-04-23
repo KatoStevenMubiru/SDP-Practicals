b5f6382b6c98b02a8efd3f37162e3feb562fcfed
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-23 06:36:19 +0300

    Update Component.java

--- Behavourial/Mediator/Component.java
+++ Behavourial/Mediator/Component.java
@@ -2,5 +2,9 @@ package Behavourial.Mediator;
 
 public abstract class Component {
     protected DialogMediator mediator;
+
+    public Component(DialogMediator mediator){
+        this.mediator = mediator;
+    }
     
 }
