88b1bf8d446d9eb0c4163f1a112b563cda8bc0d2
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-24 05:57:18 +0300

    Update Game.java

--- Behavourial/Memento/Game.java
+++ Behavourial/Memento/Game.java
@@ -2,6 +2,32 @@ package Behavourial.Memento;
 
 public class Game {
     public static void main(String[] args) {
-        
+        GameCharacter hero = new GameCharacter();
+        Caretaker caretaker = new Caretaker();
+
+        // Initial state
+        hero.setHealth(100);
+        hero.setLevel(1);
+        hero.setLocation("Starting Point");
+        caretaker.saveMemento(hero.save());
+
+        // State change
+        hero.setHealth(80);
+        hero.setLevel(2);
+        hero.setLocation("Forest");
+        caretaker.saveMemento(hero.save());
+
+        // Another state change
+        hero.setHealth(50);
+        hero.setLevel(3);
+        hero.setLocation("Mountain");
+
+        // Restore to previous state
+        hero.restore(caretaker.getMemento());
+        System.out.println("Restored to level " + hero.getLevel() + " at location " + hero.getLocation());
+
+        // Restore to initial state
+        hero.restore(caretaker.getMemento());
+        System.out.println("Restored to level " + hero.getLevel() + " at location " + hero.getLocation());
     }
 }
