280cb7e97247b5055931a887dfa3ff3ae7e3aa12
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-24 05:36:06 +0300

    Update CharacterMemento.java

--- Behavourial/Memento/CharacterMemento.java
+++ Behavourial/Memento/CharacterMemento.java
@@ -1,5 +1,14 @@
 package Behavourial.Memento;
-
+// This class holds the state of the 'GameCharacter'
 public class CharacterMemento {
+    private int health;
+    private int level;
+    private String location ;
+
+    public CharacterMemento(int health, int level, String location){
+        this.health = health;
+    }
+
+
     
 }
