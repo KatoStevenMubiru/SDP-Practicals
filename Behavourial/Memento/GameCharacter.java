81477f5fadaf2308aa186d04525ce9654580bb05
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-24 05:42:02 +0300

    Create GameCharacter.java

---
+++ Behavourial/Memento/GameCharacter.java
@@ -0,0 +1,11 @@
+package Behavourial.Memento;
+
+public class GameCharacter {
+    private int health;
+    private int level;
+    private String location;
+
+    //Setters
+    public void
+    
+}
