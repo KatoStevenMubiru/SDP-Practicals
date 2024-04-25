74e1d859ca53df3530915f1bdd0706ea1069daa6
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-25 06:30:44 +0300

    Update SubscriptionService.java

--- Behavourial/Observer/SubscriptionService.java
+++ Behavourial/Observer/SubscriptionService.java
@@ -2,5 +2,10 @@ public class SubscriptionService {
 
     public static void main(String[] args) {
         MagazinePublisher publisher = new MagazinePublisher();
+        Subscriber alice = new Reader("Alice");
+        Subscriber namagembe   = new Reader("Mildred");
+
+        publisher.subscribe(alice);
+        publisher.subscribe(namagembe);
     }
 }
