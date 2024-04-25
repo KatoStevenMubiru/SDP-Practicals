4cc498921fcdb69ec97b214e2b39ffb58552ff88
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-25 06:26:13 +0300

    Create Reader.java

---
+++ Behavourial/Observer/Reader.java
@@ -0,0 +1,11 @@
+//This defines how to handle the notifications
+public class Reader implements SUbscriber{
+
+    private String name;
+
+    private Reader(String name){
+        this.name = name;
+    }
+
+    
+}
