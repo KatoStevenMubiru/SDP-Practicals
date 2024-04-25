23cb56899523c56c6096628660241dbf2501ecc2
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-25 06:21:08 +0300

    Update MagazinePublisher.java

--- Behavourial/Observer/MagazinePublisher.java
+++ Behavourial/Observer/MagazinePublisher.java
@@ -4,5 +4,11 @@ import java.util.ArrayList;
 //This is the concrete publisher that triggers events and resp notifications
 public class MagazinePublisher {
     private List<Subscriber> subscribers = new ArrayList<>();
+    private String latestIssue;
+
+    public void publishNewIssue(String issue){
+        this.latestIssue = issue;
+        notifySubscribers();
+    }
     
 }
