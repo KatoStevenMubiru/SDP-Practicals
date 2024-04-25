189d18c06f09987362c7f30d50565b64eed4cf4e
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-25 06:15:05 +0300

    Update Publisher.java

--- Behavourial/Observer/Publisher.java
+++ Behavourial/Observer/Publisher.java
@@ -1,6 +1,9 @@
 import java.util.List;
+import java.util.concurrent.Flow.Subscriber;
 //Defines the methods any publisher must implement i.e subscribing and unsubscribing.
 public interface Publisher {
       void subscribe(Subscriber subscribe);
+      void unsubscribe(Subscriber subscribe);
+      void notifySubscribers();
     
 }
