a3b4eaf30620e613a1e8319d45fb8a883c738f04
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-26 06:12:51 +0300

    Update Smartphone.java

--- Behavourial/State/Smartphone.java
+++ Behavourial/State/Smartphone.java
@@ -1,5 +1,13 @@
 package Behavourial.State;
 
 public class Smartphone {
+    private PhoneState currentState;
+
+    public Smartphone(PhoneState state){
+        this.currentState = state;
+    }
+    public void setPhoneState(PhoneState state){
+        this.currentState = state;
+    }
     
 }
