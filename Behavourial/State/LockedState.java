be663dfd3c5254bdba67364a8f9f660dab386e1a
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-26 05:57:25 +0300

    Update LockedState.java

--- Behavourial/State/LockedState.java
+++ Behavourial/State/LockedState.java
@@ -2,6 +2,6 @@ package Behavourial.State;
 
 public class LockedState implements PhoneState{
     public void pressButton(){
-        System.out.println();
+        System.out.println("The phone is locked. Pressing the button opens the unlock screen.");
     }
 }
