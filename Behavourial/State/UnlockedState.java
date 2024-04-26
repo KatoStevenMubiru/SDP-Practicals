5d36786f14db9b38533680a561b24448dc6d2878
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-26 05:59:07 +0300

    Update UnlockedState.java

--- Behavourial/State/UnlockedState.java
+++ Behavourial/State/UnlockedState.java
@@ -1,5 +1,8 @@
 package Behavourial.State;
 
-public class UnlockedState {
+public class UnlockedState implements PhoneState {
+    public void pressButton(){
+        System.out.println("The phone is unlocked. Pressing the button opens the home screen.");
+    }
     
 }
