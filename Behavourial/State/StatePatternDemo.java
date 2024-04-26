7fe0b5f74fb5213ccc9b66b4448eed406a9bc53f
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-26 06:15:38 +0300

    Update StatePatternDemo.java

--- Behavourial/State/StatePatternDemo.java
+++ Behavourial/State/StatePatternDemo.java
@@ -3,6 +3,13 @@ package Behavourial.State;
 public class StatePatternDemo {
     public static void main(String[] args) {
         Smartphone phone = new Smartphone(new LockedState());
+
+        //Phone locked initially
+        phone.pressButton();
+
+        //change state to unlocked
+        phone.setPhoneState(new UnlockedState());
+        
     }
     
 }
