cc842d621bf9c9c9acaf870f8d337b2dfc912898
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-26 06:08:33 +0300

    Create LowBatteryState.java

---
+++ Behavourial/State/LowBatteryState.java
@@ -0,0 +1,7 @@
+package Behavourial.State;
+
+public class LowBatteryState {
+    public void pressButton(){
+        System.out.println("The phone's battery is low. Pressing the button shows the battery low alert.");
+    }
+}
