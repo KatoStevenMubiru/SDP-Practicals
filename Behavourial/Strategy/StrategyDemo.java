bd305c8d492905341b5b07f015fde72f22aa3099
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-27 05:52:05 +0300

    Update StrategyDemo.java

--- Behavourial/Strategy/StrategyDemo.java
+++ Behavourial/Strategy/StrategyDemo.java
@@ -3,6 +3,8 @@ package Behavourial.Strategy;
 public class StrategyDemo {
     public static void main(String[] args) {
         Navigator navigator = new Navigator(new DrivingStrategy());
+        
+        navigator.buildRoute("Home", "Office");
     }
     
 }
