d3b2fbb9552ceb6810911d807cdedff651db2f77
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-27 05:40:41 +0300

    Create DrivingStrategy.java

---
+++ Behavourial/Strategy/DrivingStrategy.java
@@ -0,0 +1,8 @@
+package Behavourial.Strategy;
+
+public class DrivingStrategy implements RouteStrategy {
+    public void buildRoute(String origin, String destination){
+        System.out.println("Building ");
+    }
+    
+}
