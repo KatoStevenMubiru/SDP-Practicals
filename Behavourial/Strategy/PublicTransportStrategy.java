53c6751c82d26cb9630a1a1d3df950847ddb97e8
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-27 05:46:37 +0300

    Create PublicTransportStrategy.java

---
+++ Behavourial/Strategy/PublicTransportStrategy.java
@@ -0,0 +1,8 @@
+package Behavourial.Strategy;
+
+public class PublicTransportStrategy implements RouteStrategy {
+    public void buildRoute(String origin, String destination){
+        
+    }
+    
+}
