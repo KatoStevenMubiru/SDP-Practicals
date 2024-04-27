31b071d9ca2a7ae92e52b199c05137ff13dd71ce
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-27 05:49:48 +0300

    Update Navigator.java

--- Behavourial/Strategy/Navigator.java
+++ Behavourial/Strategy/Navigator.java
@@ -6,5 +6,7 @@ public class Navigator {
     public Navigator(RouteStrategy strategy){
         this.strategy = strategy;
     }
-    
+    public void setStrategy(RouteStrategy strategy){
+        this.strategy = strategy;
+    }
 }
