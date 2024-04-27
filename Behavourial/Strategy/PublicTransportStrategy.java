1348b3e397d6813c214093d9d3d928c580538612
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-27 05:47:36 +0300

    Update PublicTransportStrategy.java

--- Behavourial/Strategy/PublicTransportStrategy.java
+++ Behavourial/Strategy/PublicTransportStrategy.java
@@ -2,7 +2,8 @@ package Behavourial.Strategy;
 
 public class PublicTransportStrategy implements RouteStrategy {
     public void buildRoute(String origin, String destination){
-        
+         System.out.println("Building a public transport route from " + origin + " to " + destination);
+
     }
     
 }
