98b95600796d1db33fd046d8518f399d8afd7c6f
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-28 05:22:28 +0300

    Update EspressoCoffee.java

--- Behavourial/Template/EspressoCoffee.java
+++ Behavourial/Template/EspressoCoffee.java
@@ -1,5 +1,14 @@
 package Behavourial.Template;
 
-public class EspressoCoffee {
-    
+public class EspressoCoffee extends CoffeeBrewer {
+
+    @Override
+    protected void brew(){
+        System.out.println("Forcing water through ground coffee");
+    }    
+
+    @Override
+    protected void addCondiments(){
+        System.out.println("No condiments added");
+    }
 }
