4fe4a47bd0db15c6287f495694b261ad5f3dc685
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-28 05:14:08 +0300

    Update CoffeeBrewer.java

--- Behavourial/Template/CoffeeBrewer.java
+++ Behavourial/Template/CoffeeBrewer.java
@@ -11,5 +11,9 @@ public class CoffeeBrewer {
     private void boilWater(){
         System.out.println("Boiling water");
     }
-    
+    protected abstract void brew();
+
+    private void pourInCup(){
+        
+    }
 }
