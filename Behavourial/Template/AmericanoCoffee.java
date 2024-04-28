f6f0b1d69ff6d748a9722ae5425162a01611533a
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-28 05:20:29 +0300

    Update AmericanoCoffee.java

--- Behavourial/Template/AmericanoCoffee.java
+++ Behavourial/Template/AmericanoCoffee.java
@@ -1,5 +1,15 @@
 package Behavourial.Template;
 
-public class AmericanoCoffee {
+public class AmericanoCoffee extends CoffeeBrewer{
+
+    @Override
+    protected void brew(){
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    @Override
+    protected void addCondiments(){
+        System.out.println("Adding sugar and cream");
+    }
     
 }
