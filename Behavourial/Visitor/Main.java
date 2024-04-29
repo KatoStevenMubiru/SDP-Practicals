1e94da50da742fedd4d301f3bf1994cb887c8b4f
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-29 03:15:07 +0300

    Update Main.java

--- Behavourial/Visitor/Main.java
+++ Behavourial/Visitor/Main.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         List<Building> buildings = Arrays.asList(new ResidentialBuilding(), new Bank(), new CoffeeShop());
-        
+        InsuranceAgent agent = new ConcreteInsuranceAgent();
+        for(Building building: buildings){
+            building.accept(agent);
+        }
     }
     
 }
