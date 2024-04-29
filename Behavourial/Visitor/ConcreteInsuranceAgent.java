30d28a1dd4c9aa02f5824d0e685d9a913b544507
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-29 03:03:04 +0300

    Update ConcreteInsuranceAgent.java

--- Behavourial/Visitor/ConcreteInsuranceAgent.java
+++ Behavourial/Visitor/ConcreteInsuranceAgent.java
@@ -2,6 +2,12 @@ package Behavourial.Visitor;
 
 public class ConcreteInsuranceAgent {
     public void visit(ResidentialBuilding residential){
+
+        System.out.println("Offering medical insurance to residential building.");
+
+    }
+
+    public void visit(Bank bank){
         
     }
 }
