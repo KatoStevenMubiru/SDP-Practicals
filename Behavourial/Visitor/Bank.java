0b088a9a4f939fa044f3313a57cd5303c1b065f2
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-29 03:08:45 +0300

    Update Bank.java

--- Behavourial/Visitor/Bank.java
+++ Behavourial/Visitor/Bank.java
@@ -1,5 +1,7 @@
 package Behavourial.Visitor;
 
-public class Bank {
-    
+public class Bank implements Building {
+    public void accept(InsuranceAgent agent){
+        agent.visit(this);
+    }
 }
