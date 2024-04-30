5679323851e3aeb3bc7b5da680d9382cea8fe555
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-30 05:36:58 +0300

    Update TimeSheetReport.java

--- SOLID/ApplyingSRP/TimeSheetReport.java
+++ SOLID/ApplyingSRP/TimeSheetReport.java
@@ -1,5 +1,13 @@
 package SOLID.ApplyingSRP;
 
-public class TimeSheetReport {
-    
+
+public class TimesheetReport {
+    public String generate(Employee employee) {
+        StringBuilder report = new StringBuilder();
+        report.append("Timesheet Report for ").append(employee.getName()).append(" (").append(employee.getId()).append(")\n");
+        for (Double hours : employee.getHoursWorked()) {
+            report.append(" - ").append(hours).append(" hours\n");
+        }
+        return report.toString();
+    }
 }
