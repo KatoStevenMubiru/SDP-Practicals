f35b9b13558c87896df7359c0e76e842093f27fc
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-30 05:32:09 +0300

    Update Employee.java

--- SOLID/ApplyingSRP/Employee.java
+++ SOLID/ApplyingSRP/Employee.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 class Employee{
     private String name;
     private String id;
     private List<Double> hoursWorked;
+
+    public Employee(String name, String id){
+        this.name = name;
+        this.id = name;
+        this.hoursWorked = new ArrayList<>();
+    }
 }
