1c4b3f62ad5ea8479c64c460aed28cab39955a66
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-04-30 05:20:54 +0300

    Create Employee.java

---
+++ SOLID/ViolatingSRP/Employee.java
@@ -0,0 +1,15 @@
+package SOLID.ViolatingSRP;
+//Here the class is Violating SRP
+public class Employee {
+    private String name;
+    private String id;
+    private List<Double> hoursWorked;
+    
+    public Employee(String name, String id){
+        this.name = name;
+        this.id = id;
+        this.hoursWorked = new
+    }
+
+    
+}
