ee76b5e06f93fafd501d9317db3f901d6a8f598c
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-05-01 06:17:49 +0300

    Create Employee.java

---
+++ SOLID/SRP/ViolatingSRP/Employee.java
@@ -0,0 +1,34 @@
+package SOLID.ViolatingSRP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Here the class is Violating SRP
+public class Employee {
+    private String name;
+    private String id;
+    private List<Double> hoursWorked;
+    
+    public Employee(String name, String id){
+        this.name = name;
+        this.id = id;
+        this.hoursWorked = new ArrayList<>();
+
+    }
+    
+    public void addHoursWorked(double hours){
+        hoursWorked.add(hours);
+    }
+
+    public String generateTimesheetReport(){
+        StringBuilder report = new StringBuilder();
+
+        report.append("Timesheet Report for ").append(name).append(" (").append(id).append(")\n");
+
+        for(Double hours : hoursWorked){
+            report.append(" - ").append(hours).append(" hours\n");
+        }
+        return report.toString();
+    }
+    
+}
