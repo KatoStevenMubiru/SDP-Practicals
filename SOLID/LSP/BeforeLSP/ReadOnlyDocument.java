9c156d4035e728f954f37c40f9d63a161370f17d
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-05-02 04:56:12 +0300

    Update ReadOnlyDocument.java

--- SOLID/LSP/BeforeLSP/ReadOnlyDocument.java
+++ SOLID/LSP/BeforeLSP/ReadOnlyDocument.java
@@ -3,7 +3,7 @@ package SOLID.LSP.BeforeLSP;
 public class ReadOnlyDocument  extends Document{
     @Override
     public void save(){
-        throw new UnsupportedOperationException()
+        throw new UnsupportedOperationException("Cannot save a read-only document.");
     }
     
 }
